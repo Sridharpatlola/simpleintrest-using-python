dc2e51c5b82fcbe2515a717adb118e4b174fec19
Sridhar Patlola <85889620+Sridharpatlola@users.noreply.github.com>
2022-12-12 20:07:31 +0530

    Add files via upload

---
+++ streamlitapp.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+options = option_menu(menu_title="",options=["Simple Interest calculator"],orientation="horizontal")
+if options == "Simple Interest calculator":
+    st.title("Simple interest calculator")
+    P = st.number_input("Enter the principal amount")
+    T = st.number_input("Enter the time in years")
+    R = st.number_input("Enter the rate of interest")
+    I = (P * T * R) / 100
+    if st.button("Calculate interest"):
+        st.success("The simple interest is {}".format(I))
+# if options == "Sales dashboard":
+#     st.warning("App is in progress")
+
+
+
+
+
